bc767190ea718586e51b638348e0c0592db765c4
martarutkowska97 <marta.rutkowska97@wp.pl>
2018-07-04 10:49:08 +0200

    initial commit

---
+++ app/src/main/java/com/example/marta/wifi_detection/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.marta.wifi_detection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
