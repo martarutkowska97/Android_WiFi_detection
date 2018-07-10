8632b7f669d7dcead8d2cfca464a98e1108415dc
Marta Rutkowska <pr_m.rutkowska@neurosys.com>
2018-07-10 20:45:22 +0200

    Documentation part 1

--- app/src/main/java/com/example/marta/wifi_detection/KalmanFilter.java
+++
@@ -1,4 +0,0 @@
-package com.example.marta.wifi_detection;
-
-public class KalmanFilter {
-}
--- app/src/main/java/com/example/marta/wifi_detection/MainActivity.java
+++ app/src/main/java/com/example/marta/wifi_detection/MainActivity.java
@@ -46,34 +46,60 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * The WiFi_detection program is used to collect data of all routers nearby
+ * by detecting them, resolving their MAC address and signal strength
+ * and link them to the coordinates, which are derived from the accelerometer.
+ *
+ * This application also saves collected data into JSON file in order to make
+ * those data more useful and prepared for further calculations and processing.
+ *
+ * This class contains main content of the Activity and basic service
+ *
+ */
 public class MainActivity extends Activity {
 
+    /**
+     * How many times the app collects data with one scan click
+     */
     public static final int NUMBER_OF_SCANS = 3;
+    /**
+     * File name of the file which is used to store data about the file counter
+     */
     public static final String FILE_COUNTER_EXTENSION="fileCounter.bin";
 
+    /**
+     * Arraylist of the objects containing information about current WiFis available nearby
+     */
     private static ArrayList<WiFiElement> content;
+    /**
+     * Adapter for the RecyclerView
+     */
     private RVAdapter adapter;
-
     private WifiManager wifiManager;
+    /**
+     * Broadcast receiver used to receive WiFi data by scanning nearby WiFis
+     */
     private WifiScanReceiver wifiScanReceiver;
-
     private int fileCounter;
-    private ArrayList<JSONObject> dataCollection;
+    /**
+     * Used to collect data from the accelerometer about the current displacement
+     */
     private DisplacementCollector displacementCollector;
 
-
-
+    /**
+     * Initialization of the activity and used services
+     * @param savedInstanceState Bundle: If the activity is being re-initialized after previously
+     * being shut down then this Bundle contains the data it most recently supplied in
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        checkAllPermssions();
-
+        checkAllPermissions();
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         content=new ArrayList<>();
-        dataCollection=new ArrayList<>();
         adapter = new RVAdapter();
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -86,10 +112,13 @@ public class MainActivity extends Activity {
 
         fileCounter = readFileCounterFromFile();
         displacementCollector=new DisplacementCollector(getApplicationContext());
-
     }
 
-    private void checkAllPermssions(){
+    /**
+     * Checks if all required permissions are granted
+     * if they are not application sends a request to the user
+     */
+    private void checkAllPermissions(){
         if(!(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
                 ContextCompat.checkSelfPermission(this, Manifest.permission.CHANGE_WIFI_STATE) == PackageManager.PERMISSION_GRANTED &&
@@ -103,6 +132,11 @@ public class MainActivity extends Activity {
         }
     }
 
+    /**
+     * Initializes button used to trigger a WiFi scan
+     * It initializes also a Thread which controls number of scans and
+     * controls starting next scan when the scanner is ready
+     */
     private void initializeScanButton(){
         Button scan= findViewById(R.id.button_scan);
         scan.setOnClickListener(new View.OnClickListener() {
@@ -140,6 +174,13 @@ public class MainActivity extends Activity {
         });
     }
 
+    /**
+     * Reads from file a value of the variable responsible for creating
+     * a new file with a specific number- called fileCounter.
+     *
+     * If file which should contain file counter does not exist, method returns 0
+     * @return integer number responsible for identifying new file
+     */
     private int readFileCounterFromFile(){
       int f=0;
       File fileDir=getApplication().getFilesDir();
@@ -168,6 +209,10 @@ public class MainActivity extends Activity {
       return f;
     }
 
+    /**
+     * Called when the activity is no longer visible to the user, because another activity
+     * has been resumed and is covering this one. This method saves to file the file counter also
+     */
     @Override
     protected void onStop() {
         super.onStop();
@@ -187,10 +232,12 @@ public class MainActivity extends Activity {
         }
     }
 
+    /**
+     * Converts the data into JSON object and saves it to files: JSON file and .txt file.
+     * @param jsonWiFiData JSONWiFiData it holds the data which are later converted to JSOn object
+     */
     private void saveToJSONFile(JSONWiFiData jsonWiFiData){
-        //JSONWiFiData jsonWiFiData=new JSONWiFiData(displacementCollector.getDisplacement()[0],displacementCollector.getDisplacement()[1],content);
         JSONObject jsonObject=jsonWiFiData.makeJSONObject();
-
 
         try (FileWriter file = new FileWriter(Environment.getExternalStorageDirectory()+"/plikJSON"+fileCounter+".json")) {
             file.write(jsonObject.toString());
@@ -213,6 +260,10 @@ public class MainActivity extends Activity {
 
     }
 
+    /**
+     * Clears the Arraylist holding data of nearby routers with their signals, then
+     * adds new data collected and notifies an adapter to refresh the RecyclerView
+     */
     private void returnAllWifis(){
         content.clear();
         List<ScanResult> scanResults = wifiManager.getScanResults();
@@ -223,28 +274,47 @@ public class MainActivity extends Activity {
         adapter.notifyDataSetChanged();
     }
 
+    /**
+     * Called when the system is about to start resuming a previous activity.
+     * Unregisters the broadcast receiver.
+     */
     @Override
     protected void onPause() {
         unregisterReceiver(wifiScanReceiver);
         super.onPause();
     }
 
+
+    /**
+     * Called when the activity will start interacting with the user.
+     * Registers the broadcast receiver.
+     */
     @Override
     protected void onResume() {
         registerReceiver(wifiScanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         super.onResume();
     }
 
+    /**
+     * BroadcastReceiver type class, responsible for listening the WiFi scanner if ready,
+     * then collecting data, updating the RecyclerView, determining the displacement and
+     * saving data collected to files.
+     */
     class WifiScanReceiver extends BroadcastReceiver{
 
+        /**
+         * Responsible for listening the WiFi scanner if ready,
+         * then collecting data, updating the RecyclerView, determining the displacement and
+         * saving data collected to files.
+         * @param c context of the application
+         * @param intent intent from which actions are established
+         */
         @Override
         public void onReceive(Context c, Intent intent) {
             if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
                 returnAllWifis();
                 float[] disp = displacementCollector.getDisplacement();
                 JSONWiFiData jsonWiFiData=new JSONWiFiData(disp[0],disp[1],content);
-                JSONObject jsonObject=jsonWiFiData.makeJSONObject();
-                dataCollection.add(jsonObject);
                 saveToJSONFile(jsonWiFiData);
                 synchronized (MainActivity.this) {
                     MainActivity.this.notifyAll();
@@ -254,14 +324,30 @@ public class MainActivity extends Activity {
     }
 
 
+    /**
+     * This class inherits from the RecyclerView.Adapter, it implements specific adapter for our
+     * RecyclerView
+     */
     class RVAdapter extends RecyclerView.Adapter<MainActivity.ViewHolder>{
 
+        /**
+         *
+         */
         LayoutInflater mLayoutInflater;
 
+        /**
+         *
+         */
         public RVAdapter(){
             mLayoutInflater=getLayoutInflater();
         }
 
+        /**
+         *
+         * @param parent
+         * @param viewType
+         * @return
+         */
         @Override
         public MainActivity.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view=mLayoutInflater.inflate(R.layout.wifi_element,parent,false);
@@ -269,30 +355,51 @@ public class MainActivity extends Activity {
             return holder;
         }
 
+        /**
+         *
+         * @param holder
+         * @param position
+         */
         @Override
         public void onBindViewHolder(MainActivity.ViewHolder holder, int position) {
             holder.addressMAC.setText(content.get(position).getAddressMAC());
             holder.signalStrength.setText(content.get(position).getSignalStrength());
         }
 
+        /**
+         *
+         * @return
+         */
         @Override
         public int getItemCount() {
             return content.size();
         }
 
-        public WiFiElement getItem(int position) {
-            return content.get(position);
-        }
     }
 
+
+    /**
+     * Class responsible for creating a view holder for a recycle view.
+     * This class inherits properties from a class RecyclerView.ViewHolder
+     *
+     */
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        /**
+         * TextView holding an information
+         */
         TextView addressMAC;
+        /**
+         * TextView holding an information about the signal strength of specific router in text
+         */
         TextView signalStrength;
 
+        /**
+         * Constructor creating an instance of a ViewHolder class, initializing TextViews
+         * @param view
+         */
         public ViewHolder(View view){
             super(view);
-
             addressMAC= view.findViewById(R.id.mac_address);
             signalStrength= view.findViewById(R.id.signal_strength);
         }
